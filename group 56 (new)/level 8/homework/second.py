4e972b2f7f51a0346a1bfa593f9b3425db455091
Davit1null <choxelidaviti@gmail.com>
2024-11-14 10:43:38 -0800

    second commit

---
+++ group 56 (new)/level 8/homework/second.py
@@ -0,0 +1,4 @@
+name = input("enter your name:")
+last_name = (input("enter your last name:")"-")
+full_name = (name + last_name)
+print("hello " + full_name)
---
+++ group 56 (new)/level 8/homework/third.py
@@ -0,0 +1,6 @@
+num1 = int(input("enter your first number"))
+num2 = int(input("enter your second number"))
+num3 = int(input("enter your third number"))
+num4 = int(input("enter your fourth number"))
+num5 = int(input("enter your fifth number"))
+print((num1 + num2) / num3 - num4*num5 % num2)
