8f6da149d1f010978fe5ec33d126237062c60dcb
Davit1null <choxelidaviti@gmail.com>
2024-11-28 12:44:15 -0800

    second commit

---
+++ group 56 (new)/level 14/homework/eight.py
@@ -0,0 +1,5 @@
+num1 = int(input("please enter your first number:"))
+num2 = int(input("please enter your second number:"))
+num3 = int(input("please enter your third number:"))
+for i in range(num1, num2, num3) :
+    print(i**2)
