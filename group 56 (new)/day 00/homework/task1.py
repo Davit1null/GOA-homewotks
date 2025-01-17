a380ff5f18d2e5dbfbc9e459ff51be7bd8204cde
Davit1null <choxelidaviti@gmail.com>
2025-01-17 08:03:54 -0800

    second commit

---
+++ group 56 (new)/day 00/homework/task1.py
@@ -0,0 +1,77 @@
+from turtle import*
+
+width (5)
+
+forward (200)
+left (90)
+forward (200)
+left (90)
+forward (200)
+left (90)
+forward (200)
+
+
+
+
+
+left (90)
+forward (170)
+
+
+color ("gray")
+
+
+left (90)
+forward (100)
+left (90)
+forward (50)
+left (90)
+forward (100)
+
+
+
+
+penup()
+goto(200, 200)
+pendown()
+
+
+color("green")
+
+right(150)
+forward(200)
+left(120)
+forward(200)
+
+
+penup()
+goto(30, 70)
+pendown()
+
+
+color("brown")
+
+left (30)
+forward (30)
+left(90)
+forward(50)
+left(90)
+forward(60)
+left(90)
+forward(50)
+left(90)
+forward(30)
+left(90)
+forward(50)
+left(90)
+forward(30)
+left(90)
+forward(25)
+left(90)
+forward(60)
+
+
+
+
+
+exitonclick()
