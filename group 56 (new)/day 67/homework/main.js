3298e1f93e69a7865b88d616ba8d4955ba89af55
Davit1null <choxelidaviti@gmail.com>
2025-06-28 12:59:16 -0700

    second commit

---
+++ group 56 (new)/day 67/homework/main.js
@@ -0,0 +1,16 @@
+let name = "David"
+let city = "Tbilisi"
+console.log(city)
+
+city = "shanhai"
+console.log(city)
+
+let number = 10
+let user_num = 3
+let user_num1 = 4
+console.log(number > (user_num + user_num1))
+
+let boolean = true
+let number1 = 67
+console.log(String(Boolean))
+console.log(String(number1))
